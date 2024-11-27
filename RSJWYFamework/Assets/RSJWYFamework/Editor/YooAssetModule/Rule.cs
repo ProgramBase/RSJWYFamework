0f8a5ac7dbc242932d21e0bb6fb5c20e7a9ef0b2
rsjwy\admin <1446176202@qq.com>
2024-11-27 14:56:09 +0800

    addd：增加打包忽略

--- RSJWYFamework/Assets/RSJWYFamework/Editor/YooAssetModule/Rule.cs
+++ RSJWYFamework/Assets/RSJWYFamework/Editor/YooAssetModule/Rule.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using UnityEditor;
 using YooAsset.Editor;
 namespace RSJWYFamework.Editor.YooAssetModule
 {
@@ -20,6 +21,43 @@ namespace RSJWYFamework.Editor.YooAssetModule
             //提取文件名，增加标记
             var filename=Path.GetFileName(data.AssetPath);
             return new PackRuleResult($"{filename}_HotCodeEncrypt", "hotcode");
+        }
+    }
+
+    public class PrefabIgnoreRule : IIgnoreRule
+    {
+        /// <summary>
+        /// 查询是否为忽略文件
+        /// </summary>
+        public bool IsIgnore(AssetInfo assetInfo)
+        {
+            if (assetInfo.AssetPath.StartsWith("Assets/") == false && assetInfo.AssetPath.StartsWith("Packages/") == false)
+            {
+                UnityEngine.Debug.LogError($"Invalid asset path : {assetInfo.AssetPath}");
+                return true;
+            }
+            // 忽略Editor文件夹下的资源
+            if (assetInfo.AssetPath.Contains("/Editor/")||assetInfo.AssetPath.Contains("Packages/"))
+                return true;
+
+            // 忽略文件夹
+            if (AssetDatabase.IsValidFolder(assetInfo.AssetPath))
+                return true;
+
+            // 忽略编辑器下的类型资源
+            if (assetInfo.AssetType == typeof(LightingDataAsset))
+                return true;
+            if (assetInfo.AssetType == typeof(LightmapParameters))
+                return true;
+
+            // 忽略Unity引擎无法识别的文件
+            if (assetInfo.AssetType == typeof(UnityEditor.DefaultAsset))
+            {
+                UnityEngine.Debug.LogWarning($"Cannot pack default asset : {assetInfo.AssetPath}");
+                return true;
+            }
+
+            return DefaultIgnoreRule.IgnoreFileExtensions.Contains(assetInfo.FileExtension);
         }
     }
 }
